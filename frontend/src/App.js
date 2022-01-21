bb6cfaa755b3bab2e6a755806fe8e187068dc428
Vlad Foroshcha <vladfo2003@gmail.com>
2022-01-21 19:04:11 +0200

    added test endpoint component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
+import React from 'react';
+import { ShowData } from './Components/ShowData';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        hello world!
+        <ShowData url="http://127.0.0.1:8000/api/ready" />
+        <ShowData url='https://jsonplaceholder.typicode.com/posts/' method='POST' body={{id: 32}} />
       </header>
     </div>
   );
