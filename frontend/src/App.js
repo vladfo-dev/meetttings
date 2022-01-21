de040f66fe93d811c18d562759b3640bc3c6b767
Vlad Foroshcha <vladfo2003@gmail.com>
2022-01-21 14:07:33 +0200

    initial bug fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,8 @@
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        hello world!
       </header>
     </div>
   );
